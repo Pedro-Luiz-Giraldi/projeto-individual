2d9561c0f6124d4a998dc6ffc78ebf4a126d4c2e
Pedro-Luiz-Giraldi <pedro.giraldi@sptech.school>
2025-05-14 22:04:23 -0300

    Páginas de cadastro, login e quiz

---
+++ funcoes.js
@@ -0,0 +1,14 @@
+function headerLinks(btn) {
+    if (btn == 'inicio') {
+        location.href = "file:///C:/Users/Pedro/Documents/SPTECH/projeto-individual/index.html"
+    }
+    if (btn == 'cadastro') {
+        location.href = "file:///C:/Users/Pedro/Documents/SPTECH/projeto-individual/cadastro.html"
+    }
+    if (btn == 'login') {
+        location.href = "file:///C:/Users/Pedro/Documents/SPTECH/projeto-individual/login.html"
+    }
+    if (btn == 'quiz') {
+        location.href = "file:///C:/Users/Pedro/Documents/SPTECH/projeto-individual/quiz.html"
+    }
+}
