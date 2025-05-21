d2f0359752725a8d43fe89da06c6293e0e7c0ea3
Pedro-Luiz-Giraldi <pedro.giraldi@sptech.school>
2025-05-20 23:12:56 -0300

    Sistema de Cadastro e Login

---
+++ web-data-viz/public/js/funcoes.js
@@ -0,0 +1,14 @@
+function headerLinks(btn) {
+    if (btn == 'inicio') {
+        location.href = "index.html"
+    }
+    if (btn == 'cadastro') {
+        location.href = "cadastro.html"
+    }
+    if (btn == 'login') {
+        location.href = "login.html"
+    }
+    if (btn == 'quiz') {
+        location.href = "quiz.html"
+    }
+}
--- web-data-viz/src/controllers/usuarioController.js
+++ web-data-viz/src/controllers/usuarioController.js
@@ -1,5 +1,4 @@
 var usuarioModel = require("../models/usuarioModel");
-var aquarioModel = require("../models/aquarioModel");
 
 function autenticar(req, res) {
     var email = req.body.emailServer;
@@ -20,32 +19,25 @@ function autenticar(req, res) {
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
+                        res.json({
+                            id: resultadoAutenticar[0].id,
+                            email: resultadoAutenticar[0].email,
+                            nome: resultadoAutenticar[0].nome,
+                            senha: resultadoAutenticar[0].senha                         
+                        });
+
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
                         res.status(403).send("Mais de um usuário com o mesmo login e senha!");
                     }
-                }
-            ).catch(
+}
+            ).catch (
                 function (erro) {
-                    console.log(erro);
-                    console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
-                    res.status(500).json(erro.sqlMessage);
-                }
+    console.log(erro);
+    console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+    res.status(500).json(erro.sqlMessage);
+}
             );
     }
 
