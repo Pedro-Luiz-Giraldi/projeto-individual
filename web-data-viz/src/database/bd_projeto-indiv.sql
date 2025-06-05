f9ba95c13acc2ff457eaac7e35c34f1db7d637d5
Pedro-Luiz-Giraldi <pedro.giraldi@sptech.school>
2025-06-04 23:52:51 -0300

    KPIs e Dados

--- web-data-viz/src/database/bd_projeto-indiv.sql
+++ web-data-viz/src/database/bd_projeto-indiv.sql
@@ -20,6 +20,20 @@ create table forum (
     foreign key (fkusuario) references usuario(id)
 );
 
+INSERT INTO usuario (nome, email, senha, fav_npc) VALUES	
+	('GiantDad', 'giantdad@gmail.com', 'giantdad', 'Siegmeyer'),
+	('Oscar', 'oscar@gmail.com', 'oscar', 'Solaire'),
+	('João', 'joao@gmail.com', 'joao', 'Laurentius'),
+	('Roberto', 'roberto@gmail.com', 'roberto', 'Patches'),
+	('André', 'andre@gmail.com', 'andre', 'Andre');
+
+INSERT INTO forum () VALUES
+	('Giant Dad', 'Armas: Chaos Zweihander +5, Pyromancy Flame\n\n\nArmadura: Mask of the Father (drop do Pinwheel ou vendida pelo Patches) \n                  Giant Armor\n\n\nAnéis: Havels Ring, FaP (Favor and Protection)\n\n\nMagias: 2x Black Flame\n\n\nSl: 99\nVit: 48\nAtt: 12\nEnd: 66\nStr: 16\nDex: 10', 'builds', '1'),
+    ('No Soul', 'Regras da run:\n  - Não upar de nível\n  - Não comprar itens\n  - Não melhorar equipamento\n  - Usar apenas itens encontrados pelo mundo\n\n\nBoa sorte.', 'challangeruns', '1'),
+    ('Ooh Shiny!', 'Regras da run:\n  - Commeçar com a classe Deprived\n  - Deve usar/equipar todo item assim que o ver\n  - Equipar armas na mão direita e escudos na mão esquerda', 'challangeruns', '1'),
+    ('Marvelous Chester', 'De alguma forma, o npc Marvelous Chester encontrado na dlc Artorias of  The Abyss, veio do universo de Bloodborne.\nEssa afirmação é baseada na semelhança do design do seu personagem com o estilo de arte do jogo, além de que durante o desenvolvimento da dlc, Miyazaki provavelmente já possuia ideias para a criação de Bloodborne. ', 'historia', '2'),
+    ('Dark Lord', 'Crown of the Great Lord\nBlack Knight Armor\nShadow Gauntlets\nWaistcloth of the Channelers', 'fashion', '2');
+
 select * from usuario;
 select * from forum;
 
@@ -28,7 +42,8 @@ SELECT f.titulo, f.comentario, f.categoria, u.nome
             JOIN usuario u
             WHERE f.fkusuario = u.id;
 
-    
--- drop table forum;
--- truncate table forum;
--- truncate table usuario;
+SELECT fav_npc FROM usuario;
+
+SELECT categoria, count(categoria) qtd
+        FROM forum
+        GROUP BY categoria;
--- web-data-viz/src/routes/forum.js
+++ web-data-viz/src/routes/forum.js
@@ -4,12 +4,10 @@ var router = express.Router();
 var forumController = require("../controllers/forumController");
 
 router.post("/salvar", function (req, res) {
-    // função a ser chamada quando acessar /carros/salvar
     forumController.salvar(req, res);
 });
 
 router.get("/exibir/:filtroServer", function (req, res) {
-    // função a ser chamada quando acessar /carros/exibir
     forumController.exibir(req, res);
 });
 
